5f580509c0950039119a7293ee896275118df71d
zjy <1604244855@qq.com>
2022-08-25 11:14:50 +0800

    update

---
+++ makefile
@@ -0,0 +1,8 @@
+path = ${abspath $(CURDIR)/build/makefile.conf}
+
+shell = $(SHELL)
+
+GCC = $(CC)
+
+all:
+	@echo $(BUILD_PATH)
--- test.sh
+++ test.sh
@@ -2,22 +2,9 @@
 
 var_01=1
 var_02=2
-function vca(){
-    if [ $var_01 -eq 2 ];then
-        return 6
-    elif [ $var_01 -eq 1 ];then
-        return 1
-    fi
-}
 
-for i in {1..7}
-do
-    vca
-    if [ $? -eq 6 ];then
-        echo "6"
-    fi
-    echo $?
-    # elif [ $? -eq 0 ];then
-    #     echo "0"
-    # fi
-done
+cat << EOF
+
+hello world
+
+EOF
