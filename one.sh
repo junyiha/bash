1e64dd2085c3ac4c57017960335606cc6a0231e2
zjy <1604244855@qq.com>
2022-12-15 16:59:12 +0800

    add link_so.sh shell which is used in nvidia docker configuration

--- one.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash +x
-
-echo "this is the one file"
-
-echo "now turn to the two file"
-
-. ./two.sh
-
--- two.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash +x
-
-echo "now we are in the two file"
-
-echo "over"
-libgomp1 libzmq5 libjsoncpp1 libhiredis0.13 libsdl2-2.0-0 liblapacke libatlas-base-dev libuuid1zlib1g
